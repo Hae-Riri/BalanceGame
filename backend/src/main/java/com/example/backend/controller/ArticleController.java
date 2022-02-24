10e7647b52db362979dca67fa9ff35c26f6942d6
0JUUU <clleo97@naver.com>
2022-02-24 17:21:42 +0900

    feat: :sparkles: create Article List GET API #45

--- backend/src/main/java/com/example/backend/controller/ArticleController.java
+++ backend/src/main/java/com/example/backend/controller/ArticleController.java
@@ -1,17 +1,24 @@
 package com.example.backend.controller;
 
+import com.example.backend.domain.Article;
+import com.example.backend.dto.ArticleGetResponse;
 import com.example.backend.dto.ArticlePostRequest;
 import com.example.backend.dto.BaseResult;
+import com.example.backend.dto.ListResult;
 import com.example.backend.service.ArticleService;
 import com.example.backend.service.ResponseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -33,5 +40,18 @@ public class ArticleController {
     public ResponseEntity<BaseResult> createArticle(@RequestBody ArticlePostRequest articlePostRequest) {
         articleService.createArticle(articlePostRequest);
         return new ResponseEntity<>(responseService.getSuccessBaseResult(), HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "게시글 목록 조회", notes = "조건에 맞는 게시글 목록을 반환한다.")
+    @GetMapping("")
+    public ResponseEntity<ListResult<ArticleGetResponse>> getArticles(@RequestParam Long offset,
+        @RequestParam(required = false) String sort, @RequestParam(required = false) String category,
+        @RequestParam(required = false) String search) {
+
+        List<Article> articles = articleService.getArticles(offset, sort, category, search);
+        List<ArticleGetResponse> articleResponses = articles.stream().map(ArticleGetResponse::of)
+            .collect(Collectors.toList());
+        ListResult<ArticleGetResponse> articleGetResponseResults = responseService.getListResult(articleResponses);
+        return new ResponseEntity<>(articleGetResponseResults, HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/example/backend/service/ArticleService.java
+++ backend/src/main/java/com/example/backend/service/ArticleService.java
@@ -5,6 +5,8 @@ import com.example.backend.domain.ArticleCategory;
 import com.example.backend.dto.ArticlePostRequest;
 import com.example.backend.repository.ArticleCategoryRepository;
 import com.example.backend.repository.ArticleRepository;
+import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class ArticleService {
+
+    private static final int PAGE_SIZE = 10;
 
     private final ArticleRepository articleRepository;
     private final ArticleCategoryRepository articleCategoryRepository;
@@ -23,5 +27,53 @@ public class ArticleService {
             articlePostRequest.getArticleCategoryName()).orElseThrow();
         Article article = articlePostRequest.toEntity(articleCategory);
         articleRepository.save(article);
+    }
+
+    public List<Article> getArticles(Long offset, String sort, String categoryName, String search) {
+        if (Objects.isNull(offset)) {
+            // TODO: 에러 처리
+        }
+
+        Article article = articleRepository.findById(offset).orElseThrow();
+
+        if (Objects.isNull(sort) && Objects.isNull(categoryName) && Objects.isNull(search)) {
+            return articleRepository.findPerPageBeforeOrderByCreatedAt(article.getCreatedAt(), PAGE_SIZE);
+        }
+
+        if (Objects.isNull(categoryName) && Objects.isNull(search)) {
+            return articleRepository.findPerPageBeforeOrderByTotalCount(article.getCreatedAt(), PAGE_SIZE);
+        }
+
+        if (Objects.isNull(sort) && Objects.isNull(search)) {
+            ArticleCategory articleCategory = articleCategoryRepository.findByName(categoryName).orElseThrow();
+            return articleRepository.findPerPageBeforeByCategoryOrderByCreatedAt(article.getCreatedAt(),
+                articleCategory.getId(), PAGE_SIZE);
+        }
+
+        if (Objects.isNull(sort) && Objects.isNull(categoryName)) {
+            return articleRepository.findPerPageBeforeBySearchOrderByCreatedAt(article.getCreatedAt(),
+                "%" + search + "%", PAGE_SIZE);
+        }
+
+        if (Objects.isNull(search)) {
+            ArticleCategory articleCategory = articleCategoryRepository.findByName(categoryName).orElseThrow();
+            return articleRepository.findPerPageBeforeByCategoryOrderByTotalCount(article.getCreatedAt(),
+                articleCategory.getId(), PAGE_SIZE);
+        }
+
+        if (Objects.isNull(categoryName)) {
+            return articleRepository.findPerPageBeforeBySearchOrderByTotalCount(article.getCreatedAt(),
+                "%" + search + "%", PAGE_SIZE);
+        }
+
+        if (Objects.isNull(sort)) {
+            ArticleCategory articleCategory = articleCategoryRepository.findByName(categoryName).orElseThrow();
+            return articleRepository.findPerPageBeforeByCategoryBySearchOrderByCreatedAt(article.getCreatedAt(),
+                articleCategory.getId(), "%" + search + "%", PAGE_SIZE);
+        }
+
+        ArticleCategory articleCategory = articleCategoryRepository.findByName(categoryName).orElseThrow();
+        return articleRepository.findPerPageBeforeByCategoryBySearchOrderByTotalCount(article.getCreatedAt(),
+            articleCategory.getId(), "%" + search + "%", PAGE_SIZE);
     }
 }
